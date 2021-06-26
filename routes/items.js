dc4fc94599c476af6e7f9cf58b6f1f02da9d3b8e
novailoveyou <novailoveyoux@gmail.com>
2021-06-26 12:28:03 +0300

    Add Item POST

--- routes/items.js
+++ routes/items.js
@@ -1,4 +1,4 @@
-const { getItems, getItem } = require('../controllers/items')
+const { getItems, getItem, addItem } = require('../controllers/items')
 
 // Item schema
 const Item = {
@@ -31,12 +31,24 @@ const getItemOpts = {
   handler: getItem
 }
 
+const postItemOpts = {
+  schema: {
+    response: {
+      201: Item
+    }
+  },
+  handler: addItem
+}
+
 function itemRoutes(fastify, options, done) {
   // Get all items
   fastify.get('/items', getItemsOpts)
 
   // Get single item
   fastify.get('/items/:id', getItemOpts)
+
+  // Add item
+  fastify.post('/items', postItemOpts)
 
   done()
 }
